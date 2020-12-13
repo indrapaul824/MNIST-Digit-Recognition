79a1a5556414727ed21bd5955bb176cbf24a2f1c
INDRASHIS PAUL <64627762+IndraP24@users.noreply.github.com>
2020-12-13 21:53:04 +0530

    Updated LinearSVC model

--- Machine Learning Approach/svm.py
+++ Machine Learning Approach/svm.py
@@ -2,8 +2,6 @@ import numpy as np
 from sklearn.svm import LinearSVC
 
 
-### Functions for you to fill in ###
-
 def one_vs_rest_svm(train_x, train_y, test_x):
     """
     Trains a linear SVM for binary classifciation
@@ -15,7 +13,10 @@ def one_vs_rest_svm(train_x, train_y, test_x):
     Returns:
         pred_test_y - (m,) NumPy array containing the labels (0 or 1) for each test data point
     """
-    raise NotImplementedError
+    svm = LinearSVC(C=0.1, random_state=0)
+    svm.fit(train_x, train_y)
+    pred_test_y = svm.predict(test_x)
+    return pred_test_y
 
 
 def multi_class_svm(train_x, train_y, test_x):
@@ -29,7 +30,10 @@ def multi_class_svm(train_x, train_y, test_x):
     Returns:
         pred_test_y - (m,) NumPy array containing the labels (int) for each test data point
     """
-    raise NotImplementedError
+    svm = LinearSVC(C=0.1, random_state=0)
+    svm.fit(train_x, train_y)
+    pred_test_y = svm.predict(test_x)
+    return pred_test_y
 
 
 def compute_test_error_svm(test_y, pred_test_y):
