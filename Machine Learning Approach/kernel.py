9a31b34f23138ea381bf958a79f48b7dd577a0a4
INDRASHIS PAUL <64627762+IndraP24@users.noreply.github.com>
2020-12-14 23:31:06 +0530

    Added GaussianRBF kernel function

--- Machine Learning Approach/kernel.py
+++ Machine Learning Approach/kernel.py
@@ -37,5 +37,10 @@ def rbf_kernel(X, Y, gamma):
         Returns:
             kernel_matrix - (n, m) Numpy array containing the kernel matrix
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    XTX = np.mat([np.dot(row, row) for row in X]).T
+    YTY = np.mat([np.dot(row, row) for row in Y]).T
+    XTX_matrix = np.repeat(XTX, Y.shape[0], axis=1)
+    YTY_matrix = np.repeat(YTY, X.shape[0], axis=1).T
+    K = np.asarray((XTX_matrix + YTY_matrix - 2 * (X @ Y.T)), dtype='float64')
+    K *= - gamma
+    return np.exp(K, K)
